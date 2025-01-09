7d37c507fc7b67af5f57adaceacbd803f4b667e7
MakAlex90 <meskalin4ik9@gmail.com>
2025-01-10 01:43:54 +0300

    Delete suite.py

--- suite.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import tests_12_3
-ranST = unittest.TestSuite()
-ranST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
-ranST.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
-runner = unittest.TextTestRunner(verbosity=2)
-runner.run(ranST)
